76daed98cf4ac969b4c55122cd8ac0c0fab2f877
El0cean <lummeelmeri@gmail.com>
2023-03-03 12:36:20 +0100

    alternative client

--- src/client.java
+++ src/client.java
@@ -113,7 +113,8 @@ public class client {
           PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
           BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-          System.out.println("Sending username...\n");
+          System.out.println("list\n");
+          
           out.println(msg[0]);
           while (socket.isConnected()) {
               String input;
